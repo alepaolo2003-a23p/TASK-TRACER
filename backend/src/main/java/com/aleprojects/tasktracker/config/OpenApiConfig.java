0dd6c49debc4b4bf3d43586c5412a39c710515f9
aleeeeeeeeeeeeeeee <alepaolo.2003@gmail.com>
2026-07-22 22:42:59 -0500

    fix: agregar server URL dinamico en OpenAPI para Swagger

--- backend/src/main/java/com/aleprojects/tasktracker/config/OpenApiConfig.java
+++ backend/src/main/java/com/aleprojects/tasktracker/config/OpenApiConfig.java
@@ -5,15 +5,21 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    @Value("${API_BASE_URL:http://localhost:8080}")
+    private String apiBaseUrl;
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url(apiBaseUrl))
                 .info(new Info()
                         .title("Task Tracker API")
                         .version("1.0")
